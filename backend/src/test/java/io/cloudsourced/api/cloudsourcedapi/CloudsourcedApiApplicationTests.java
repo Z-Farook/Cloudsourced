954cb3c1665fbc67fd6bb74a20e324623dbaf3ff
Joost Stam <joost.stam@gmail.com>
2020-04-15 09:51:35 +0200

    Add java backend

---
+++ backend/src/test/java/io/cloudsourced/api/cloudsourcedapi/CloudsourcedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cloudsourced.api.cloudsourcedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudsourcedApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
