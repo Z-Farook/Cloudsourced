39721dc8c81240e995899ebf2f7580ca275e1df8
Dennis Kievits <denkievits@gmail.com>
2020-04-17 23:51:36 +0200

    Added basic database docker-compose with and .env setup

--- frontend/src/routing/MainRouter.tsx
+++ frontend/src/routing/MainRouter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route, BrowserRouter as Router} from "react-router-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 
 interface IProps {}
