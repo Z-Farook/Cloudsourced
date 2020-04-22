44a1a6f4fdd7d8c840467bcec49195168db48625
JorisvdSluis <0946664@hr.nl>
2020-04-22 16:33:03 +0200

    Added basic projectCardComponent

--- frontend/src/components/Projects/ProjectCardComponent.tsx
+++ frontend/src/components/Projects/ProjectCardComponent.tsx
@@ -1,14 +1,36 @@
 import React from "react";
 
 
-import { Layout } from "antd";
-
+import { Card, Avatar } from "antd";
+import {SettingOutlined, EditOutlined, EllipsisOutlined, MoreOutlined, HeartFilled, ShareAltOutlined, DownOutlined} from '@ant-design/icons';
+import Meta from "antd/lib/card/Meta";
 interface IProps {}
 
 const ProjectCardComponent: React.FC<IProps> = (props) => {
   return (
-  <>
-  </>
+ <Card
+    style={{ width: 300 }}
+    cover={
+      <img
+        alt="example"
+        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+      />
+    }
+    actions={[
+      <HeartFilled key="favorite" />,
+      <ShareAltOutlined key="share" />,
+      <DownOutlined key="ellipsis" />,
+    ]}
+    title="Projectname"
+    extra={<MoreOutlined />}
+    
+  >
+    <Meta
+      avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+      title="Typescript"
+      description="This is the description of a project."
+    />
+  </Card>
   );
 };
 
--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { RouteComponentProps } from "react-router";
 import DefaultLayout from "../../components/layout/DefaultLayout";
 import { Typography } from "antd";
+import ProjectCardComponent from "../../components/Projects/ProjectCardComponent";
 
 interface IProps extends RouteComponentProps {}
 
@@ -11,6 +12,7 @@ const HomePage: React.FC<IProps> = (props) => {
       <div style={{ padding: 20 }}>
         <Typography.Title>Home</Typography.Title>
       </div>
+      <ProjectCardComponent></ProjectCardComponent>
     </DefaultLayout>
   );
 };
