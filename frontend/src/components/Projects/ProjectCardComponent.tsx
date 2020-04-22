a4fdc9d2bdd74bbba0695761df0d8b77f9609693
JorisvdSluis <0946664@hr.nl>
2020-04-22 15:23:49 +0200

    Add new component

---
+++ frontend/src/components/Projects/ProjectCardComponent.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+import { Layout } from "antd";
+
+interface IProps {}
+
+const ProjectCardComponent: React.FC<IProps> = (props) => {
+  return (
+  <>
+  </>
+  );
+};
+
+export default ProjectCardComponent;
