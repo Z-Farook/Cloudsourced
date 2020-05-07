da7e4f7df53df65550e25dbc57f1bbcd93f01a40
Joost Stam <joost.stam@gmail.com>
2020-05-07 20:58:03 +0200

    Modify layout

--- frontend/src/pages/ProjectDetailPage/index.tsx
+++ frontend/src/pages/ProjectDetailPage/index.tsx
@@ -23,13 +23,13 @@ const ProjectDetailPage: React.FC<IProps> = (props) => {
   const projectId = parseInt(props.match.params.projectId);
   const { title, description, imageSource } = projectMocks[projectId];
 
-  const data = ({
-      name: "Basic login form",
-      points: 100,
-      description:
-        "We want a login form that takes an email and a password, with validation and the ability to submit the form.",
-      codeLanguage: "tsx",
-      codePreview: `interface IProps {
+  const data = {
+    name: "Basic login form",
+    points: 100,
+    description:
+      "We want a login form that takes an email and a password, with validation and the ability to submit the form.",
+    codeLanguage: "tsx",
+    codePreview: `interface IProps {
         // These fields can be filled so they need to be used as default values
         emailAddress?: string;
         password?: string;
@@ -41,39 +41,48 @@ const ProjectDetailPage: React.FC<IProps> = (props) => {
             // Please implement
         );
       };`,
-  });
+  };
 
-
-
-
-  
+  // TODO: make pretty 
   return (
     <DefaultLayout>
       <div style={{ padding: 20 }}>
         <Typography.Title>{title}</Typography.Title>
         <Row>
-          <Col span={12}>
-            <img
-              alt="example"
-              className="image"
-              src={
-                imageSource
-                  ? imageSource
-                  : "https://source.unsplash.com/400x300/?code,pc"
-              }
-            />
+          <Col span={12} offset={6}>
+            <Row>
+              <Col span={12}>
+                <img
+                  alt="example"
+                  className="image"
+                  src={
+                    imageSource
+                      ? imageSource
+                      : "https://source.unsplash.com/400x300/?code,pc"
+                  }
+                />
+              </Col>
+              <Col span={12}>
+                <Divider
+                  orientation="left"
+                  style={{ color: "#333", fontWeight: "normal" }}
+                >
+                  Description
+                </Divider>
+                    {description}
+              </Col>
+            </Row>
+
+            {/* TODO: map through project features */}
+            <Divider
+                  orientation="left"
+                  style={{ color: "#333", fontWeight: "normal" }}
+                >
+                  Features
+              </Divider>
+            <FeatureCard {...data}></FeatureCard>
           </Col>
         </Row>
-        <Divider
-          orientation="left"
-          style={{ color: "#333", fontWeight: "normal" }}
-        >
-          Description
-        </Divider>
-        <Row>
-          <Col span={24}>{description}</Col>
-        </Row>
-        <FeatureCard {...data}></FeatureCard>
       </div>
     </DefaultLayout>
   );
