31cb577124cc30ca4ad5c33c498665a6e26dfbe9
Gavin den hollander <gavindhollander@gmail.com>
2020-05-13 21:19:52 +0200

    Pom and user entity

--- backend/src/main/java/io/cloudsourced/api/cloudsourcedapi/Entity/User.java
+++ backend/src/main/java/io/cloudsourced/api/cloudsourcedapi/Entity/User.java
@@ -12,5 +12,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
+    private String email;
+    private String password;
 }
