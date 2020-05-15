546c711e1eb9ed70a5fdf9b23badb35d3cef37d0
Zahid <0950085@hr.nl>
2020-05-15 10:34:49 +0200

    Removed some imports
    form TransactionService

--- backend/src/main/java/io/cloudsourced/api/cloudsourcedapi/Service/TransactionService.java
+++ backend/src/main/java/io/cloudsourced/api/cloudsourcedapi/Service/TransactionService.java
@@ -1,11 +1,7 @@
 package io.cloudsourced.api.cloudsourcedapi.Service;
 
-import io.cloudsourced.api.cloudsourcedapi.Default.Exception.NotFoundException;
 import io.cloudsourced.api.cloudsourcedapi.Entity.Transaction;
-import io.cloudsourced.api.cloudsourcedapi.Entity.User;
 import io.cloudsourced.api.cloudsourcedapi.Persistence.TransactionRepository;
-
-import java.util.Optional;
 
 @org.springframework.stereotype.Service
 public class TransactionService extends BaseService<Transaction, TransactionRepository> {
