785318867a3e68c508ea11b9e4283e482fc4499f
Joost Stam <joost.stam@gmail.com>
2020-05-24 18:26:26 +0200

    add cors support for features

--- backend/src/main/java/io/cloudsourced/api/cloudsourcedapi/API/Resource/FeatureResource.java
+++ backend/src/main/java/io/cloudsourced/api/cloudsourcedapi/API/Resource/FeatureResource.java
@@ -5,6 +5,7 @@ import io.cloudsourced.api.cloudsourcedapi.Persistence.FeatureRepository;
 import io.cloudsourced.api.cloudsourcedapi.Service.FeatureService;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/feature")
 public class FeatureResource extends BaseResource<Feature, FeatureService, FeatureRepository>{
