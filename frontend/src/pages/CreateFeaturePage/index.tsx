f0f26f5032b211c1ff47f126fbda58e48d8b9c15
Joost Stam <joost.stam@gmail.com>
2020-05-24 18:32:32 +0200

    add form to create features

--- frontend/src/pages/CreateFeaturePage/index.tsx
+++ frontend/src/pages/CreateFeaturePage/index.tsx
@@ -1,26 +1,66 @@
 import React from "react";
-import { Input, Button, Row, Col, message } from "antd";
-import { useForm, Controller, ErrorMessage } from "react-hook-form";
+import { Form, Input, Button, Row, Col, Select, message } from "antd";
 import DefaultLayout from "../../components/layout/DefaultLayout";
-
 import Title from "antd/lib/typography/Title";
 
 import {
-  ProjectResourceApi,
-  AddWithUserUsingPOSTRequest,
+  AddFeatureToProjectUsingPOSTRequest,
+  FeatureResourceApi,
 } from "cloudsourced-api";
 import { RouteComponentProps } from "react-router";
-
-interface IProps extends RouteComponentProps {}
+import { Store } from "antd/lib/form/interface";
+interface IRouterParams {
+  projectId: string;
+}
+interface IProps extends RouteComponentProps<IRouterParams> {}
 
 type Inputs = {
-  projectName: string;
+  featureName: string;
   description: string;
+  codePreview: string;
+  codeLanguage: string;
 };
-
+const { Option } = Select;
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
 const CreateFeaturePage: React.FC<IProps> = (props) => {
+  const projectId = +props.match.params.projectId;
+  const handleSubmit = async (values: Store) => {
+    console.log(values);
+    const params: AddFeatureToProjectUsingPOSTRequest = {
+      projectId: projectId,
+      feature: {
+        name: values.featureName,
+        description: values.description,
+        codeLanguage: values.codeLanguage,
+        codePreview: values.codePreview,
+      },
+    };
+    message.loading({ content: "Saving project...", key: "updatableKey" });
+    await new FeatureResourceApi()
+      .addFeatureToProjectUsingPOST(params)
+      .then(() => {
+        message.success({
+          content: "Feature is created succesfully!",
+          key: "updatableKey",
+          duration: 2,
+        });
+        props.history.push(`/projects/${projectId}`);
+      })
+      .catch(() => {
+        errorMessage();
+      });
+  };
 
-
+  const errorMessage = () => {
+    message.error({
+      content: "Something went wrong",
+      key: "updatableKey",
+      duration: 2,
+    });
+  };
   return (
     <DefaultLayout>
       <div style={{ backgroundColor: "#f5f5f5" }}>
@@ -29,7 +69,66 @@ const CreateFeaturePage: React.FC<IProps> = (props) => {
             <Col xl={8} lg={12} md={12} sm={24} xs={24}>
               <Title>Create a new project</Title>
 
+              <Form {...layout} onFinish={handleSubmit}>
+                <Form.Item
+                  label="Feature name"
+                  name="featureName"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input the feature name!",
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
 
+                <Form.Item
+                  label="Description"
+                  name="description"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input the description!",
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+
+                <Form.Item
+                  name="codeLanguage"
+                  label="Language"
+                  rules={[{ required: true }]}
+                >
+                  <Select
+                    placeholder="Select a option and change input text above"
+                    allowClear
+                  >
+                    <Option value="typescript">TypeScript</Option>
+                    <Option value="csharp">C#</Option>
+                  </Select>
+                </Form.Item>
+
+                <Form.Item
+                  label="Code preview"
+                  name="codePreview"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input the code preview!",
+                    },
+                  ]}
+                >
+                  <Input.TextArea />
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                  <Button type="primary" htmlType="submit">
+                    Create
+                  </Button>
+                </Form.Item>
+              </Form>
             </Col>
           </Row>
         </div>
