e1df329210aeae6f560f727b51d450cf6d793493
JorisvdSluis <0946664@hr.nl>
2020-05-27 13:57:47 +0200

    added working image upload

--- frontend/src/pages/CreateProjectPage/index.tsx
+++ frontend/src/pages/CreateProjectPage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input, Button, Row, Col, message, Upload } from "antd";
 import { useForm, Controller, ErrorMessage } from "react-hook-form";
 import DefaultLayout from "../../components/layout/DefaultLayout";
@@ -20,9 +20,9 @@ type Inputs = {
 
 const CreateProjectPage: React.FC<IProps> = (props) => {
   const { control, handleSubmit, errors } = useForm<Inputs>();
+  const [image, setImage] = useState("");
 
-  const postImage = (image: string) => {
-    // POST request using fetch inside useEffect React hook
+  const postImage = async (image: string): Promise<string> => {
     image = image.split("base64,")[1];
     const requestOptions = {
       method: "POST",
@@ -35,12 +35,18 @@ const CreateProjectPage: React.FC<IProps> = (props) => {
         type: "base64",
       }),
     };
-    fetch("https://api.imgur.com/3/image", requestOptions)
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-
-    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+    const response = await fetch(
+      "https://api.imgur.com/3/image",
+      requestOptions
+    );
+    const data = await response.json();
+    return data.data.link;
   };
+
+  const renderImage = (image: string) => {
+    setImage(image);
+  };
+
   const handleProject = async (data: Inputs) => {
     const params: AddWithUserUsingPOSTRequest = {
       id: 1,
@@ -48,7 +54,7 @@ const CreateProjectPage: React.FC<IProps> = (props) => {
         description: data.description,
         user: {},
         name: data.projectName,
-        image: "https://source.unsplash.com/500x500/?coding,pc",
+        image: await postImage(image),
       },
     };
     message.loading({ content: "Saving project...", key: "updatableKey" });
@@ -67,19 +73,17 @@ const CreateProjectPage: React.FC<IProps> = (props) => {
         errorMessage();
       });
   };
-  const getBase64 = (img: Blob, callback: any) => {
+  const getBase64 = (img: Blob) => {
     const reader = new FileReader();
-    reader.addEventListener("load", () => postImage(reader.result as string));
+
+    reader.addEventListener("load", () => {
+      //postImage(reader.result as string);
+      renderImage(reader.result as string);
+    });
+
     reader.readAsDataURL(img);
   };
 
-  let imgUrl;
-
-  const uploadButton = (
-    <div>
-      <div className="ant-upload-text">Upload</div>
-    </div>
-  );
   const errorMessage = () => {
     message.error({
       content: "Something went wrong",
@@ -128,22 +132,11 @@ const CreateProjectPage: React.FC<IProps> = (props) => {
                   listType="picture-card"
                   className="avatar-uploader"
                   showUploadList={false}
-                  action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
                   onChange={(event) =>
-                    getBase64(
-                      event.file.originFileObj as Blob,
-                      (res: string) => {
-                        imgUrl = res;
-                        console.log(res);
-                      }
-                    )
+                    getBase64(event.file.originFileObj as Blob)
                   }
                 >
-                  {imgUrl ? (
-                    <img src={imgUrl} alt="avatar" style={{ width: "100%" }} />
-                  ) : (
-                    <img src={imgUrl} alt="avatar" style={{ width: "100%" }} />
-                  )}
+                  <img src={image} alt="avatar" style={{ width: "100%" }} />
                 </Upload>
                 <Button type="primary" htmlType="submit" block>
                   Submit
