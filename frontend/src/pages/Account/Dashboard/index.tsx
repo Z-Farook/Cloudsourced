9bc78debcbfcd4deb9d6e1cb54a46073666449b5
JorisvdSluis <0946664@hr.nl>
2020-05-29 12:56:39 +0200

    add menu state

---
+++ frontend/src/pages/Account/Dashboard/index.tsx
@@ -0,0 +1,237 @@
+import React from "react";
+import { RouteComponentProps, withRouter } from "react-router";
+
+import {
+  Row,
+  Button,
+  PageHeader,
+  Menu,
+  Col,
+  Table,
+  Card,
+  Statistic,
+  Timeline,
+  Progress,
+} from "antd";
+
+import {
+  PlusOutlined,
+  SettingOutlined,
+  ArrowUpOutlined,
+  ArrowDownOutlined,
+} from "@ant-design/icons";
+import Title from "antd/lib/typography/Title";
+
+interface IProps extends RouteComponentProps {}
+const dataSource = [
+  {
+    key: "1",
+    number: 1,
+    project: "test project",
+  },
+  {
+    key: "2",
+    number: 2,
+    project: "test project",
+  },
+  {
+    key: "3",
+    number: 3,
+    project: "test project",
+  },
+];
+const dataSourceTasks = [
+  {
+    key: "1",
+    number: 1,
+    project: "test task",
+  },
+  {
+    key: "2",
+    number: 2,
+    project: "test task",
+  },
+  {
+    key: "3",
+    number: 3,
+    project: "test task",
+  },
+];
+const dataSourceTransactions = [
+  {
+    key: "1",
+    number: 1,
+    project: "test transaction",
+  },
+  {
+    key: "2",
+    number: 2,
+    project: "test transaction",
+  },
+  {
+    key: "3",
+    number: 3,
+    project: "test transaction",
+  },
+];
+const columns = [
+  {
+    title: "#",
+    dataIndex: "number",
+    key: "number",
+  },
+  {
+    title: "Current projects",
+    dataIndex: "project",
+    key: "project",
+  },
+];
+const columnsTasks = [
+  {
+    title: "#",
+    dataIndex: "number",
+    key: "number",
+  },
+  {
+    title: "Current Tasks",
+    dataIndex: "project",
+    key: "project",
+  },
+];
+const columnsTransactions = [
+  {
+    title: "#",
+    dataIndex: "number",
+    key: "number",
+  },
+  {
+    title: "Latest transactions",
+    dataIndex: "project",
+    key: "project",
+  },
+];
+const handleMenuClick = (e: any) => {
+  console.log("click ", e);
+};
+const Dashboard: React.FC<IProps> = (props) => {
+  return (
+    <>
+      <Row justify="center" gutter={[24, 24]}>
+        <Col span={16}>
+          <Row justify="center" gutter={[24, 24]}>
+            <Col span={8}>
+              <Card>
+                <Statistic
+                  title="Projects"
+                  value={30.57}
+                  precision={2}
+                  valueStyle={{ color: "#3f8600" }}
+                  prefix={<ArrowUpOutlined />}
+                  suffix="%"
+                />
+              </Card>
+            </Col>
+            <Col span={8}>
+              <Card>
+                <Statistic
+                  title="Tasks"
+                  value={19.21}
+                  precision={2}
+                  valueStyle={{ color: "#cf1322" }}
+                  prefix={<ArrowDownOutlined />}
+                  suffix="%"
+                />
+              </Card>
+            </Col>
+            <Col span={8}>
+              <Card>
+                <Statistic
+                  title="Points"
+                  value={11.28}
+                  precision={2}
+                  valueStyle={{ color: "#3f8600" }}
+                  prefix={<ArrowUpOutlined />}
+                  suffix="%"
+                />
+              </Card>
+            </Col>
+          </Row>
+          <Row justify="center" gutter={[24, 24]}>
+            <Col span={24}>
+              <Card>
+                <Title>Progress</Title>
+                <br />
+                <div>
+                  <Title level={4}>Closed projects</Title>
+                  <Progress percent={50} status="active" />
+                </div>
+                <div>
+                  <Title level={4}>Closed tasks</Title>
+                  <Progress percent={70} status="active" />
+                </div>
+                <div>
+                  <Title level={4}>Closed points</Title>
+                  <Progress percent={100} />
+                </div>
+              </Card>
+            </Col>
+          </Row>
+        </Col>
+        <Col span={6}>
+          <Card>
+            <Title>Latest project</Title>
+            <Timeline>
+              <Timeline.Item color="green">
+                Create a services site 2015-09-01
+              </Timeline.Item>
+              <Timeline.Item color="green">
+                Create a services site 2015-09-01
+              </Timeline.Item>
+              <Timeline.Item color="red">
+                <p>Solve initial network problems 1</p>
+                <p>Solve initial network problems 2</p>
+                <p>Solve initial network problems 3 2015-09-01</p>
+              </Timeline.Item>
+              <Timeline.Item>
+                <p>Technical testing 1</p>
+                <p>Technical testing 2</p>
+                <p>Technical testing 3</p>
+              </Timeline.Item>
+            </Timeline>
+          </Card>
+        </Col>
+      </Row>
+      <Row justify="center" gutter={[24, 24]}>
+        <Col span={8}>
+          <Card>
+            <Table
+              pagination={false}
+              dataSource={dataSource}
+              columns={columns}
+            />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card>
+            <Table
+              pagination={false}
+              dataSource={dataSourceTasks}
+              columns={columnsTasks}
+            />
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card>
+            <Table
+              pagination={false}
+              dataSource={dataSourceTransactions}
+              columns={columnsTransactions}
+            />
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default withRouter(Dashboard);
--- frontend/src/pages/Account/index.tsx
+++ frontend/src/pages/Account/index.tsx
@@ -1,95 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { RouteComponentProps, withRouter } from "react-router";
 import DefaultLayout from "../../components/layout/DefaultLayout";
-import {
-  Row,
-  Button,
-  PageHeader,
-  Menu,
-  Col,
-  Table,
-  Card,
-  Statistic,
-  Timeline,
-  Progress,
-} from "antd";
+import { Row, Button, PageHeader, Menu, Col } from "antd";
 
-import {
-  PlusOutlined,
-  SettingOutlined,
-  ArrowUpOutlined,
-  ArrowDownOutlined,
-} from "@ant-design/icons";
-import Title from "antd/lib/typography/Title";
+import { PlusOutlined, SettingOutlined } from "@ant-design/icons";
+
+import Dashboard from "./Dashboard";
 
 interface IProps extends RouteComponentProps {}
-const dataSource = [
-  {
-    key: "1",
-    number: 1,
-    project: "test project",
-  },
-  {
-    key: "2",
-    number: 2,
-    project: "test project",
-  },
-  {
-    key: "3",
-    number: 3,
-    project: "test project",
-  },
-  {
-    key: "4",
-    number: 4,
-    project: "test project",
-  },
-  {
-    key: "5",
-    number: 5,
-    project: "test project",
-  },
-  {
-    key: "6",
-    number: 6,
-    project: "test project",
-  },
-  {
-    key: "7",
-    number: 7,
-    project: "test project",
-  },
-];
-const dataSourceTasks = [
-  {
-    key: "1",
-    number: 1,
-    project: "test task",
-  },
-  {
-    key: "2",
-    number: 2,
-    project: "test task",
-  },
-  {
-    key: "3",
-    number: 3,
-    project: "test task",
-  },
-];
-const columns = [
-  {
-    title: "#",
-    dataIndex: "number",
-    key: "number",
-  },
-  {
-    title: "Project",
-    dataIndex: "project",
-    key: "project",
-  },
-];
+
 const AccountPage: React.FC<IProps> = (props) => {
+  const [menu, setMenuState] = useState<"1">();
+  const handleMenuClick = (e: any) => {
+    setMenuState(e.key);
+  };
   return (
     <DefaultLayout>
       <PageHeader
@@ -109,7 +33,7 @@ const AccountPage: React.FC<IProps> = (props) => {
         <Row>
           <Col span={4}>
             <Menu
-              // onClick={this.handleClick}
+              onClick={handleMenuClick}
               style={{ width: 256, height: "100%" }}
               defaultSelectedKeys={["1"]}
               defaultOpenKeys={["sub1"]}
@@ -129,120 +53,7 @@ const AccountPage: React.FC<IProps> = (props) => {
             </Menu>
           </Col>
           <Col span={20}>
-            <Row justify="center" gutter={[24, 24]}>
-              <Col span={16}>
-                <Row justify="center" gutter={[24, 24]}>
-                  <Col span={8}>
-                    <Card>
-                      <Statistic
-                        title="Projects"
-                        value={30.57}
-                        precision={2}
-                        valueStyle={{ color: "#3f8600" }}
-                        prefix={<ArrowUpOutlined />}
-                        suffix="%"
-                      />
-                    </Card>
-                  </Col>
-                  <Col span={8}>
-                    <Card>
-                      <Statistic
-                        title="Tasks"
-                        value={19.21}
-                        precision={2}
-                        valueStyle={{ color: "#cf1322" }}
-                        prefix={<ArrowDownOutlined />}
-                        suffix="%"
-                      />
-                    </Card>
-                  </Col>
-                  <Col span={8}>
-                    <Card>
-                      <Statistic
-                        title="Points"
-                        value={11.28}
-                        precision={2}
-                        valueStyle={{ color: "#3f8600" }}
-                        prefix={<ArrowUpOutlined />}
-                        suffix="%"
-                      />
-                    </Card>
-                  </Col>
-                </Row>
-                <Row justify="center" gutter={[24, 24]}>
-                  <Col span={24}>
-                    <Card>
-                      <Title>Progress</Title>
-                      <br />
-                      <div>
-                        <Title level={4}>Closed projects</Title>
-                        <Progress percent={50} status="active" />
-                      </div>
-                      <div>
-                        <Title level={4}>Closed tasks</Title>
-                        <Progress percent={70} status="active" />
-                      </div>
-                      <div>
-                        <Title level={4}>Closed points</Title>
-                        <Progress percent={100} />
-                      </div>
-                    </Card>
-                  </Col>
-                </Row>
-              </Col>
-              <Col span={6}>
-                <Card>
-                  <Title>Latest project</Title>
-                  <Timeline>
-                    <Timeline.Item color="green">
-                      Create a services site 2015-09-01
-                    </Timeline.Item>
-                    <Timeline.Item color="green">
-                      Create a services site 2015-09-01
-                    </Timeline.Item>
-                    <Timeline.Item color="red">
-                      <p>Solve initial network problems 1</p>
-                      <p>Solve initial network problems 2</p>
-                      <p>Solve initial network problems 3 2015-09-01</p>
-                    </Timeline.Item>
-                    <Timeline.Item>
-                      <p>Technical testing 1</p>
-                      <p>Technical testing 2</p>
-                      <p>Technical testing 3</p>
-                    </Timeline.Item>
-                  </Timeline>
-                </Card>
-              </Col>
-            </Row>
-            <Row justify="center" gutter={[24, 24]}>
-              <Col span={8}>
-                <Card>
-                  <Table
-                    pagination={false}
-                    dataSource={dataSourceTasks}
-                    columns={columns}
-                  />
-                </Card>
-              </Col>
-              <Col span={8}>
-                <Card>
-                  <Table
-                    pagination={false}
-                    dataSource={dataSourceTasks}
-                    columns={columns}
-                  />
-                </Card>
-              </Col>
-              <Col span={6}>
-                <Card>
-                  <Table
-                    pagination={false}
-                    dataSource={dataSourceTasks}
-                    columns={columns}
-                  />
-                </Card>
-              </Col>
-            </Row>
+          { menu.state == "1" ? <Dashboard /> : }
           </Col>
         </Row>
       </div>
