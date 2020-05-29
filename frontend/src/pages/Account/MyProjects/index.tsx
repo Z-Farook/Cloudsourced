e10cad66a5d3ec1463b358be44388d9a9779199f
JorisvdSluis <0946664@hr.nl>
2020-05-29 13:08:47 +0200

    add projects

--- frontend/src/pages/Account/MyProjects/index.tsx
+++ frontend/src/pages/Account/MyProjects/index.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { RouteComponentProps, withRouter } from "react-router";
 import { Button } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
+import { Col, Row, Spin, Typography } from "antd";
+import { Project, ProjectResourceApi } from "cloudsourced-api";
+import IRemoteData, {
+  EState,
+  fromLoaded,
+  fromLoading,
+} from "../../../core/IRemoteData";
+import ProjectCard from "../../ProjectPage/ProjectCard/";
 
+interface IProps extends RouteComponentProps {}
 interface IProps extends RouteComponentProps {}
 
 const MyProjects: React.FC<IProps> = (props) => {
+  const [projects, setProjects] = useState<IRemoteData<Project[], null>>(
+    fromLoading()
+  );
+
+  useEffect(() => {
+    (async () => {
+      const result = await new ProjectResourceApi().allUsingGET1();
+      console.log(result);
+      setProjects(fromLoaded(result));
+    })();
+  }, []);
+
   return (
     <>
       <Button
@@ -15,6 +36,24 @@ const MyProjects: React.FC<IProps> = (props) => {
       >
         <PlusOutlined />
       </Button>
+      <div style={{ padding: 20 }}>
+        <Typography.Title>My projects</Typography.Title>
+      </div>
+      {projects.state === EState.Loading ? (
+        <Spin />
+      ) : (
+        <div className="Grid" style={{ padding: 20 }}>
+          <Row justify="center" gutter={[24, 24]}>
+            {projects.data!.map((v, i) => {
+              return (
+                <Col xl={5} lg={6} md={8} sm={12} xs={24} key={i}>
+                  <ProjectCard project={v} />
+                </Col>
+              );
+            })}
+          </Row>
+        </div>
+      )}
     </>
   );
 };
