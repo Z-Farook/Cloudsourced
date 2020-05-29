79d3e986aac7b2c0634376b9b16a79f9b8909b5a
JorisvdSluis <0946664@hr.nl>
2020-05-29 12:23:20 +0200

    Add Timeline

--- frontend/src/pages/Account/index.tsx
+++ frontend/src/pages/Account/index.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import { RouteComponentProps, withRouter } from "react-router";
 import DefaultLayout from "../../components/layout/DefaultLayout";
-import { Row, Button, PageHeader, Menu, Col, Table, Card } from "antd";
+import {
+  Row,
+  Button,
+  PageHeader,
+  Menu,
+  Col,
+  Table,
+  Card,
+  Statistic,
+  Timeline,
+} from "antd";
 
-import { PlusOutlined, SettingOutlined } from "@ant-design/icons";
+import {
+  PlusOutlined,
+  SettingOutlined,
+  ArrowUpOutlined,
+  ArrowDownOutlined,
+} from "@ant-design/icons";
+import Title from "antd/lib/typography/Title";
 
 interface IProps extends RouteComponentProps {}
 const dataSource = [
@@ -22,8 +38,44 @@ const dataSource = [
     number: 3,
     project: "test project",
   },
+  {
+    key: "4",
+    number: 4,
+    project: "test project",
+  },
+  {
+    key: "5",
+    number: 5,
+    project: "test project",
+  },
+  {
+    key: "6",
+    number: 6,
+    project: "test project",
+  },
+  {
+    key: "7",
+    number: 7,
+    project: "test project",
+  },
 ];
-
+const dataSourceTasks = [
+  {
+    key: "1",
+    number: 1,
+    project: "test task",
+  },
+  {
+    key: "2",
+    number: 2,
+    project: "test task",
+  },
+  {
+    key: "3",
+    number: 3,
+    project: "test task",
+  },
+];
 const columns = [
   {
     title: "#",
@@ -82,33 +134,76 @@ const AccountPage: React.FC<IProps> = (props) => {
                 <Row justify="center" gutter={[24, 24]}>
                   <Col span={8}>
                     {" "}
-                    <Card title="Current Projects" bordered={false}>
-                      10002
+                    <Card>
+                      <Statistic
+                        title="Projects"
+                        value={30.57}
+                        precision={2}
+                        valueStyle={{ color: "#3f8600" }}
+                        prefix={<ArrowUpOutlined />}
+                        suffix="%"
+                      />
                     </Card>
                   </Col>
                   <Col span={8}>
                     {" "}
-                    <Card title="Current tasks" bordered={false}>
-                      124555
+                    <Card>
+                      <Statistic
+                        title="Tasks"
+                        value={19.21}
+                        precision={2}
+                        valueStyle={{ color: "#cf1322" }}
+                        prefix={<ArrowDownOutlined />}
+                        suffix="%"
+                      />
                     </Card>
                   </Col>
                   <Col span={8}>
                     {" "}
-                    <Card title="Points" bordered={false}>
-                      12324456
+                    <Card>
+                      <Statistic
+                        title="Points"
+                        value={11.28}
+                        precision={2}
+                        valueStyle={{ color: "#3f8600" }}
+                        prefix={<ArrowUpOutlined />}
+                        suffix="%"
+                      />
                     </Card>
                   </Col>
                 </Row>
                 <Row justify="center" gutter={[24, 24]}>
-                  graph
+                  <Col span={24}>
+                    <Table
+                      pagination={false}
+                      dataSource={dataSourceTasks}
+                      columns={columns}
+                    />
+                  </Col>
                 </Row>
               </Col>
               <Col span={6}>
-                <Table
-                  pagination={false}
-                  dataSource={dataSource}
-                  columns={columns}
-                />
+                <Card>
+                  <Title>Latest project</Title>
+                  <Timeline>
+                    <Timeline.Item color="green">
+                      Create a services site 2015-09-01
+                    </Timeline.Item>
+                    <Timeline.Item color="green">
+                      Create a services site 2015-09-01
+                    </Timeline.Item>
+                    <Timeline.Item color="red">
+                      <p>Solve initial network problems 1</p>
+                      <p>Solve initial network problems 2</p>
+                      <p>Solve initial network problems 3 2015-09-01</p>
+                    </Timeline.Item>
+                    <Timeline.Item>
+                      <p>Technical testing 1</p>
+                      <p>Technical testing 2</p>
+                      <p>Technical testing 3 2015-09-01</p>
+                    </Timeline.Item>
+                  </Timeline>
+                </Card>
               </Col>
             </Row>
             <Row justify="center" gutter={[24, 24]}>
@@ -116,7 +211,7 @@ const AccountPage: React.FC<IProps> = (props) => {
                 {" "}
                 <Table
                   pagination={false}
-                  dataSource={dataSource}
+                  dataSource={dataSourceTasks}
                   columns={columns}
                 />
               </Col>
@@ -124,7 +219,7 @@ const AccountPage: React.FC<IProps> = (props) => {
                 {" "}
                 <Table
                   pagination={false}
-                  dataSource={dataSource}
+                  dataSource={dataSourceTasks}
                   columns={columns}
                 />
               </Col>
@@ -132,7 +227,7 @@ const AccountPage: React.FC<IProps> = (props) => {
                 {" "}
                 <Table
                   pagination={false}
-                  dataSource={dataSource}
+                  dataSource={dataSourceTasks}
                   columns={columns}
                 />
               </Col>
