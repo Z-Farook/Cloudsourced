673cf03de5b1137dc4f9de2e222b16e83c837df5
Zahid <0950085@hr.nl>
2020-06-03 11:13:24 +0200

    Small change

--- backend/src/test/java/io/cloudsourced/api/cloudsourcedapi/service/TransactionServiceTest.java
+++ backend/src/test/java/io/cloudsourced/api/cloudsourcedapi/service/TransactionServiceTest.java
@@ -6,20 +6,19 @@ import io.cloudsourced.api.cloudsourcedapi.Persistence.TransactionRepository;
 import io.cloudsourced.api.cloudsourcedapi.Service.TransactionService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-@RunWith(JUnitPlatform.class)
 public class TransactionServiceTest {
 
     @InjectMocks
