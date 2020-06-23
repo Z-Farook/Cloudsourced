30fa0122d2358ae0510b29d5abbda1d1218e9f57
Joost Stam <joost.stam@gmail.com>
2020-06-23 15:51:38 +0200

    Redesign project detail page, project detail section

--- frontend/src/pages/ProjectDetailPage/index.tsx
+++ frontend/src/pages/ProjectDetailPage/index.tsx
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { RouteComponentProps } from "react-router";
 import DefaultLayout from "../../components/layout/DefaultLayout";
-import { Typography, Row, Col, Divider, Spin, Button } from "antd";
+import {
+  Row,
+  Col,
+  Divider,
+  Spin,
+  Button,
+  Card,
+  Typography,
+  message,
+} from "antd";
 import noImage from "../../assets/noimage.png";
 import FeatureCard from "../../components/feature/FeatureCard";
 import IRemoteData, {
@@ -12,6 +21,8 @@ import IRemoteData, {
 import { ProjectDetailDTO, ProjectResourceApi } from "cloudsourced-api";
 import { api } from "../../core/api";
 import AuthStore from "../../stores/AuthStore";
+import "./index.scss";
+const { Title, Text, Paragraph } = Typography;
 
 interface IRouterParams {
   projectId: string;
@@ -44,36 +55,57 @@ const ProjectDetailPage: React.FC<IProps> = (props) => {
     })();
   }, [projectId]);
 
-  const { description, image, name: projectName } = project.data || {};
+  const { description, image, name: projectName, user } = project.data || {};
 
   return (
     <DefaultLayout>
       {project.state === EState.Loading ? (
         <Spin />
       ) : (
-        <div style={{ padding: 20 }}>
-          <Typography.Title>{projectName}</Typography.Title>
-          <Row>
-            <Col span={12} offset={6}>
-              <Row>
-                <Col span={12}>
-                  <img
-                    alt="example"
-                    className="image"
-                    src={image ? image : noImage}
-                  />
-                </Col>
-                <Col span={12}>
-                  <Divider
-                    orientation="left"
-                    style={{ color: "#333", fontWeight: "normal" }}
-                  >
-                    Description
-                  </Divider>
-                  {description}
+        <div
+          style={{
+            padding: 0,
+            backgroundColor: "#eaeaea",
+            minHeight: "calc(100vh - 46px)",
+          }}
+        >
+          <Row
+            style={{ width: "1000px", marginLeft: "auto", marginRight: "auto" }}
+          >
+            <Col
+              span={24}
+              style={{
+                backgroundColor: "white",
+                minHeight: "calc(100vh - (46px))",
+                padding: "20px 60px",
+              }}
+            >
+              <Row gutter={[0, 20]}>
+                <Col span={24}>
+                  <Title>{projectName}</Title>
+                  <Text className="subtitle">
+                    By{" "}
+                    <span onClick={() => message.info("TODO: go to user")}>
+                      {user?.name}
+                    </span>
+                  </Text>
                 </Col>
               </Row>
-
+              <Card>
+                <Row gutter={30}>
+                  <Col span={8}>
+                    <img
+                      alt="example"
+                      className="detailImage"
+                      src={image ? image : noImage}
+                    />
+                  </Col>
+                  <Col span={12}>
+                    <Title level={2}>Description</Title>
+                    <Paragraph>{description}</Paragraph>
+                  </Col>
+                </Row>
+              </Card>
               {project.data!.features!.length > 0 ? (
                 <Divider
                   orientation="left"
@@ -84,7 +116,6 @@ const ProjectDetailPage: React.FC<IProps> = (props) => {
               ) : (
                 ""
               )}
-
               {project.data!.features!.map((v, i) => {
                 return (
                   <FeatureCard
@@ -93,7 +124,6 @@ const ProjectDetailPage: React.FC<IProps> = (props) => {
                   ></FeatureCard>
                 );
               })}
-
               {isOwner ? (
                 <Button
                   onClick={() =>
