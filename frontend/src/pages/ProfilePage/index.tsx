8b0b998edd551a3edaf9ed6dfe6b8904c19a0467
JorisvdSluis <0946664@hr.nl>
2020-06-24 12:25:58 +0200

    added api calls

--- frontend/src/pages/ProfilePage/index.tsx
+++ frontend/src/pages/ProfilePage/index.tsx
@@ -10,6 +10,8 @@ import {
   Card,
   Typography,
   message,
+  BackTop,
+  Descriptions,
 } from "antd";
 import noImage from "../../assets/noimage.png";
 import FeatureCard from "../../components/feature/FeatureCard";
@@ -18,11 +20,18 @@ import IRemoteData, {
   fromLoading,
   EState,
 } from "../../core/IRemoteData";
-import { ProjectDetailDTO, ProjectResourceApi } from "cloudsourced-api";
+import {
+  ProjectDetailDTO,
+  ProjectResourceApi,
+  UserResourceApi,
+  User,
+  ProjectDTO,
+} from "cloudsourced-api";
 import { api } from "../../core/api";
 import AuthStore from "../../stores/AuthStore";
 import "./index.scss";
-const { Title, Text, Paragraph } = Typography;
+import ProjectCard from "../ProjectPage/ProjectCard";
+const { Title } = Typography;
 
 interface IRouterParams {
   id: string;
@@ -31,102 +40,104 @@ interface IRouterParams {
 export interface IProps extends RouteComponentProps<IRouterParams> {}
 
 const ProfilePage: React.FC<IProps> = (props) => {
-  const { auth } = AuthStore.useContainer();
-  console.log(auth);
-  const userId = auth?.userId;
-  const projectId = Number(props.match.params.id);
-
-  const [project, setProject] = useState<IRemoteData<ProjectDetailDTO, null>>(
+  const [user, setUser] = useState<IRemoteData<User, null>>(fromLoading());
+  const [projects, setProjects] = useState<IRemoteData<ProjectDTO[], null>>(
     fromLoading()
   );
+  useEffect(() => {
+    (async () => {
+      const result = await new UserResourceApi(
+        api.config
+      ).getUserProfileInfoUsingGET({ id: Number(props.match.params.id) });
+      setUser(fromLoaded(result));
+    })();
+  }, []);
 
-  const [isOwner, setIsOwner] = useState<boolean>(false);
+  const sort = (value: string) => {
+    if (value === "Ascending") {
+      setProjects(
+        fromLoaded(
+          projects.data!.sort((a, b) => {
+            return a.createdAt!.getTime() - b.createdAt!.getTime();
+          })
+        )
+      );
+    } else {
+      setProjects(
+        fromLoaded(
+          projects.data!.sort((a, b) => {
+            return b.createdAt!.getTime() - a.createdAt!.getTime();
+          })
+        )
+      );
+    }
+  };
 
   useEffect(() => {
     (async () => {
       const result = await new ProjectResourceApi(
         api.config
-      ).getProjectDetailByIdUsingGET({
-        id: projectId,
-      });
-      console.log(result);
-      if (userId === result.user?.id) {
-        setIsOwner(true);
-      }
-      setProject(fromLoaded(result));
-    })();
-  }, [projectId]);
+      ).getProjectsByUserIdUsingGET({ id: Number(props.match.params.id) });
 
-  const { description, image, name: projectName, user } = project.data || {};
+      setProjects(fromLoaded(result));
+    })();
+  }, []);
 
   return (
     <DefaultLayout>
-      {project.state === EState.Loading ? (
+      {user.state === EState.Loading ? (
         <Spin />
       ) : (
         <div
           style={{
             padding: 0,
             backgroundColor: "#eaeaea",
-            minHeight: "calc(100vh - 46px)",
           }}
         >
-          <Row style={{ margin: "5em" }}>
+          <Row style={{ margin: "10em" }}>
             <Col
               span={24}
               style={{
                 backgroundColor: "white",
-                minHeight: "calc(100vh - (46px))",
+                minHeight: "calc(100vh - (46px) - 20em)",
                 padding: "20px 60px",
               }}
             >
-              <Row gutter={[0, 20]}>
-                <Col span={24}>
-                  <Title>{projectName}</Title>
-                  <Text className="subtitle">
-                    By{" "}
-                    <span onClick={() => message.info("TODO: go to user")}>
-                      {user?.name}
-                    </span>
-                  </Text>
-                </Col>
-              </Row>
               <Row className="project-info">
                 <Col span={8}>
-                  <img
-                    alt="example"
-                    className="detailImage"
-                    src={image ? image : noImage}
-                  />
+                  <img alt="example" className="detailImage" src={noImage} />
                 </Col>
                 <Col span={12}>
-                  <Title level={2}>Description</Title>
-                  <Paragraph>{description}</Paragraph>
+                  <Title level={2}>
+                    {user.data!.name!}
+                    {user.data!.infix!} {user.data!.lastName!}
+                  </Title>
+                  <Descriptions>
+                    <Descriptions.Item span={16} label="Country">
+                      {user.data?.country}
+                    </Descriptions.Item>
+                    <Descriptions.Item span={16} label="Email">
+                      {user.data?.email}
+                    </Descriptions.Item>
+                  </Descriptions>
                 </Col>
               </Row>
-              {project.data!.features!.length > 0 ? (
-                <Title level={2}>Features</Title>
+              <Title level={2}>Projects</Title>
+              {projects.state === EState.Loading ? (
+                <Spin />
               ) : (
-                ""
-              )}
-              {project.data!.features!.map((v, i) => {
-                return (
-                  <FeatureCard
-                    key={i}
-                    {...{ data: v, projectId }}
-                  ></FeatureCard>
-                );
-              })}
-              {isOwner ? (
-                <Button
-                  onClick={() =>
-                    props.history.push(`/projects/${projectId}/feature/add`)
-                  }
-                >
-                  Create feature
-                </Button>
-              ) : (
-                ""
+                <div className="Grid" style={{ padding: 20 }}>
+                  <Row justify="center" gutter={[24, 24]}>
+                    {projects.data!.map((v, i) => {
+                      return (
+                        <Col xl={5} lg={6} md={8} sm={12} xs={24} key={i}>
+                          <ProjectCard project={v} />
+                        </Col>
+                      );
+                    })}
+                    <BackTop />
+                  </Row>
+                </div>
               )}
             </Col>
           </Row>
