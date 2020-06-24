710950995323f957515a3fd47567e3ae80792538
Zahid <0950085@hr.nl>
2020-06-24 16:29:29 +0200

    getAllTransactionForAUser
    This contains all The base code for Transaction and getAllTransactionForAUser().

--- backend/src/main/java/io/cloudsourced/api/cloudsourcedapi/Persistence/TransactionRepository.java
+++ backend/src/main/java/io/cloudsourced/api/cloudsourcedapi/Persistence/TransactionRepository.java
@@ -1,9 +1,14 @@
 package io.cloudsourced.api.cloudsourcedapi.Persistence;
 
 import io.cloudsourced.api.cloudsourcedapi.Entity.Transaction;
+import io.cloudsourced.api.cloudsourcedapi.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByUser(User user);
 }
--- backend/src/main/java/io/cloudsourced/api/cloudsourcedapi/Service/TransactionService.java
+++ backend/src/main/java/io/cloudsourced/api/cloudsourcedapi/Service/TransactionService.java
@@ -1,12 +1,31 @@
 package io.cloudsourced.api.cloudsourcedapi.Service;
 
 import io.cloudsourced.api.cloudsourcedapi.Default.Authentication.AuthenticatedUserBean;
+import io.cloudsourced.api.cloudsourcedapi.Default.Exception.NotFoundException;
 import io.cloudsourced.api.cloudsourcedapi.Entity.Transaction;
+import io.cloudsourced.api.cloudsourcedapi.Entity.User;
 import io.cloudsourced.api.cloudsourcedapi.Persistence.TransactionRepository;
+import io.cloudsourced.api.cloudsourcedapi.Persistence.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @org.springframework.stereotype.Service
 public class TransactionService extends BaseService<Transaction, TransactionRepository> {
-    public TransactionService(TransactionRepository repository, AuthenticatedUserBean authenticatedUserProvider) {
-        super(repository, authenticatedUserProvider);
+
+    private final UserRepository userRepository;
+
+    public TransactionService(TransactionRepository repository, UserRepository userRepository, AuthenticatedUserBean authenticatedUserBean) {
+        super(repository, authenticatedUserBean);
+        this.userRepository = userRepository;
+    }
+
+    public List<Transaction> getTransactionForUser(Long userId) {
+        Optional<User> user = userRepository.findById(userId);
+
+        if (!user.isPresent()) {
+            throw new NotFoundException("No user Found with userId: " + userId);
+        }
+        return repository.findByUser(user.get());
     }
 }
