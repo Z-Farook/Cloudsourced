c005fac2ab20b05d5e377ac9e7258e56299a9cde
JorisvdSluis <0946664@hr.nl>
2020-06-24 14:55:10 +0200

    delete unused cors

--- backend/src/main/java/io/cloudsourced/api/cloudsourcedapi/API/Resource/ReviewResource.java
+++ backend/src/main/java/io/cloudsourced/api/cloudsourcedapi/API/Resource/ReviewResource.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "http://localhost:3000")
+
 @RestController
 @RequestMapping("/review")
 public class ReviewResource extends BaseResource<Review, ReviewDTO, ReviewService, ReviewRepository, ReviewMapper>{
