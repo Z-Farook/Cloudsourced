705b62ddce31d2a95dbacc9293f16b5313c00feb
JorisvdSluis <0946664@hr.nl>
2020-06-26 23:21:02 +0200

    added 1 working test and 1 weird test

--- frontend/src/pages/ProjectDetailPage/index.test.tsx
+++ frontend/src/pages/ProjectDetailPage/index.test.tsx
@@ -12,7 +12,7 @@ import { Router } from "react-router";
 import { MainSwitch } from "../../routing/MainRouter";
 import { act } from "react-dom/test-utils";
 
-test("the right amount of items", () => {});
+// test("the right amount of items", () => {});
 
 describe("projecDetailPage", () => {
   it("Should pass", async () => {
@@ -30,41 +30,106 @@ describe("projecDetailPage", () => {
           dispatchEvent: jest.fn(),
         })),
       });
-    });
 
-    const dataContextCreator = (
-      config?: Configuration
-    ): Partial<IResources> => {
-      return {
-        project: {
-          getProjectDetail: async (
-            params: IProjectDetailParams
-          ): Promise<IProjectDetailResult> => {
-            return {
-              project: { name: "piet", id: 1 },
-            };
-          },
-        } as any,
+      const dataContextCreator = (
+        config?: Configuration
+      ): Partial<IResources> => {
+        return {
+          project: {
+            getProjectDetail: async (
+              params: 1
+            ): Promise<IProjectDetailResult> => {
+              return {
+                project: {
+                  name: "piet",
+                  id: 1,
+                  features: [],
+                  description: "test",
+                  image: "../../noimage.png",
+                },
+              };
+            },
+          } as any,
+        };
       };
-    };
 
-    const history = createMemoryHistory();
-    history.push("projects/1");
-    const wrapper = mount(
-      <DataContext.Provider value={dataContextCreator as any}>
-        <AuthStore.Provider>
-          <Router history={history}>
-            <MainSwitch />
-          </Router>
-        </AuthStore.Provider>
-      </DataContext.Provider>
-    );
+      const history = createMemoryHistory();
+      history.push("projects/1");
+      const wrapper = mount(
+        <DataContext.Provider value={dataContextCreator as any}>
+          <AuthStore.Provider>
+            <Router history={history}>
+              <MainSwitch />
+            </Router>
+          </AuthStore.Provider>
+        </DataContext.Provider>
+      );
+      await new Promise((resolve) => setImmediate(resolve));
+      // await wrapper.update();
+      const heading = await wrapper
+        .update()
+        .find(".ant-page-header-heading-title")
+        .getDOMNode();
 
-    // console.log(wrapper.debug);
+      expect(heading).toBe("piet");
+    });
+  });
+  it("Should pass and navigate", async () => {
+    await act(async () => {
+      Object.defineProperty(window, "matchMedia", {
+        writable: true,
+        value: jest.fn().mockImplementation((query) => ({
+          matches: false,
+          media: query,
+          onchange: null,
+          addListener: jest.fn(), // deprecated
+          removeListener: jest.fn(), // deprecated
+          addEventListener: jest.fn(),
+          removeEventListener: jest.fn(),
+          dispatchEvent: jest.fn(),
+        })),
+      });
 
-    expect(
-      await wrapper.update().find(".ant-page-header-heading-title").getDOMNode()
-        .nodeValue
-    ).toBe("piet");
+      const dataContextCreator = (
+        config?: Configuration
+      ): Partial<IResources> => {
+        return {
+          project: {
+            getProjectDetail: async (
+              params: 1
+            ): Promise<IProjectDetailResult> => {
+              return {
+                project: {
+                  name: "piet",
+                  id: 1,
+                  features: [],
+                  description: "test",
+                  image: "../../noimage.png",
+                },
+              };
+            },
+          } as any,
+        };
+      };
+
+      const history = createMemoryHistory();
+      history.push("projects/1");
+      const wrapper = mount(
+        <DataContext.Provider value={dataContextCreator as any}>
+          <AuthStore.Provider>
+            <Router history={history}>
+              <MainSwitch />
+            </Router>
+          </AuthStore.Provider>
+        </DataContext.Provider>
+      );
+      await new Promise((resolve) => setImmediate(resolve));
+      await wrapper.update();
+
+      const editButton = wrapper.find(".anticon-edit");
+      await editButton.simulate("click");
+
+      expect(history.location.pathname).toBe("/projects/1/edit");
+    });
   });
 });
