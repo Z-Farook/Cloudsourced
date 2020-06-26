0b97589cfdd43b9a00885db98637fb5661293d43
Joost Stam <joost.stam@gmail.com>
2020-06-26 21:54:23 +0200

    title test in de onweer

--- frontend/src/pages/ProjectDetailPage/index.test.tsx
+++ frontend/src/pages/ProjectDetailPage/index.test.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Configuration } from "../../../gen/api/dist";
-import { IResources } from "../../core/DataContext";
+import DataContext, { IResources } from "../../core/DataContext";
+import project, {
+  IProjectDetailParams,
+  IProjectDetailResult,
+} from "../../core/DataContext/project";
+import { createMemoryHistory } from "history";
+import { mount } from "enzyme";
+import AuthStore from "../../stores/AuthStore";
+import { Router } from "react-router";
+import { MainSwitch } from "../../routing/MainRouter";
+import { act } from "react-dom/test-utils";
 
 test("the right amount of items", () => {});
 
 describe("projecDetailPage", () => {
   it("Should pass", async () => {
-    await (async () => {
+    await act(async () => {
       Object.defineProperty(window, "matchMedia", {
         writable: true,
         value: jest.fn().mockImplementation((query) => ({
@@ -21,5 +31,40 @@ describe("projecDetailPage", () => {
         })),
       });
     });
+
+    const dataContextCreator = (
+      config?: Configuration
+    ): Partial<IResources> => {
+      return {
+        project: {
+          getProjectDetail: async (
+            params: IProjectDetailParams
+          ): Promise<IProjectDetailResult> => {
+            return {
+              project: { name: "piet", id: 1 },
+            };
+          },
+        } as any,
+      };
+    };
+
+    const history = createMemoryHistory();
+    history.push("projects/1");
+    const wrapper = mount(
+      <DataContext.Provider value={dataContextCreator as any}>
+        <AuthStore.Provider>
+          <Router history={history}>
+            <MainSwitch />
+          </Router>
+        </AuthStore.Provider>
+      </DataContext.Provider>
+    );
+
+    // console.log(wrapper.debug);
+
+    expect(
+      await wrapper.update().find(".ant-page-header-heading-title").getDOMNode()
+        .nodeValue
+    ).toBe("piet");
   });
 });
