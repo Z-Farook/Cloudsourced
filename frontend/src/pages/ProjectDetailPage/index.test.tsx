ed153acc22530a9fc3ca2f766372d24321c1f58b
Joost Stam <joost.stam@gmail.com>
2020-06-26 15:16:00 +0200

    setup testing for projectDetailPage

---
+++ frontend/src/pages/ProjectDetailPage/index.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Configuration } from "../../../gen/api/dist";
+import { IResources } from "../../core/DataContext";
+
+test("the right amount of items", () => {});
+
+describe("projecDetailPage", () => {
+  const dataContextCreator = (config?: Configuration): Partial<IResources> => {
+    return {
+      // authentication: {
+      //     re
+      // }
+    };
+  };
+});
