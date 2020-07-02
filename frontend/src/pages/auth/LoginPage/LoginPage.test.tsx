482ccb4774dd4b3766597be868632d87113a93fb
JorisvdSluis <0946664@hr.nl>
2020-07-02 11:33:17 +0200

    change testing order

--- frontend/src/pages/auth/LoginPage/LoginPage.test.tsx
+++ frontend/src/pages/auth/LoginPage/LoginPage.test.tsx
@@ -44,6 +44,44 @@ const dataContextCreator = (config?: Configuration): Partial<IResources> => {
   };
 };
 describe("loginPage", () => {
+  it("Should fail and has error", async () => {
+    await act(async () => {
+      Object.defineProperty(window, "matchMedia", {
+        writable: true,
+        value: jest.fn().mockImplementation((query) => ({
+          matches: false,
+          media: query,
+          onchange: null,
+          addListener: jest.fn(),
+          removeListener: jest.fn(),
+          addEventListener: jest.fn(),
+          removeEventListener: jest.fn(),
+          dispatchEvent: jest.fn(),
+        })),
+      });
+
+      const history = createMemoryHistory();
+      history.push("auth/login");
+      const wrapper = mount(
+        <DataContext.Provider value={dataContextCreator as any}>
+          <AuthStore.Provider>
+            <Router history={history}>
+              <MainSwitch />
+            </Router>
+          </AuthStore.Provider>
+        </DataContext.Provider>
+      );
+
+      const submitButton = wrapper.find("button[type='submit']");
+
+      await submitButton.simulate("click", {
+        preventDefault() {},
+      });
+      await new Promise((resolve) => setImmediate(resolve));
+
+      expect(wrapper.update().exists(".ant-form-item-has-error")).toEqual(true);
+    });
+  });
   it("Should pass and login", async () => {
     await act(async () => {
       Object.defineProperty(window, "matchMedia", {
@@ -52,8 +90,8 @@ describe("loginPage", () => {
           matches: false,
           media: query,
           onchange: null,
-          addListener: jest.fn(), // deprecated
-          removeListener: jest.fn(), // deprecated
+          addListener: jest.fn(),
+          removeListener: jest.fn(),
           addEventListener: jest.fn(),
           removeEventListener: jest.fn(),
           dispatchEvent: jest.fn(),
@@ -84,58 +122,6 @@ describe("loginPage", () => {
     });
   });
 
-  it("Should fail on validate", async () => {
-    await act(async () => {
-      Object.defineProperty(window, "matchMedia", {
-        writable: true,
-        value: jest.fn().mockImplementation((query) => ({
-          matches: false,
-          media: query,
-          onchange: null,
-          addListener: jest.fn(), // deprecated
-          removeListener: jest.fn(), // deprecated
-          addEventListener: jest.fn(),
-          removeEventListener: jest.fn(),
-          dispatchEvent: jest.fn(),
-        })),
-      });
-      const dataContextCreator = (
-        config?: Configuration
-      ): Partial<IResources> => {
-        return {
-          authentication: {
-            authenticateUser: async (
-              params: IAuthenticateUserParams
-            ): Promise<IAuthenticateUserResult> => {
-              return {
-                authentication: {},
-              };
-            },
-          } as any,
-        };
-      };
-      const history = createMemoryHistory();
-      history.push("auth/login");
-      const wrapper = mount(
-        <DataContext.Provider value={dataContextCreator as any}>
-          <AuthStore.Provider>
-            <Router history={history}>
-              <MainSwitch />
-            </Router>
-          </AuthStore.Provider>
-        </DataContext.Provider>
-      );
-      const submitButton = wrapper.find("button[type='submit']");
-
-      wrapper.update();
-      submitButton.simulate("click", {
-        preventDefault() {},
-      });
-      await new Promise((resolve) => setImmediate(resolve));
-
-      expect(wrapper.update().exists(".ant-form-item-has-error")).toEqual(true);
-    });
-  });
   // it("Should fail on email validation", async () => {
   //   await act(async () => {
   //     Object.defineProperty(window, "matchMedia", {
@@ -144,8 +130,8 @@ describe("loginPage", () => {
   //         matches: false,
   //         media: query,
   //         onchange: null,
-  //         addListener: jest.fn(), // deprecated
-  //         removeListener: jest.fn(), // deprecated
+  //         addListener: jest.fn(),
+  //         removeListener: jest.fn(),
   //         addEventListener: jest.fn(),
   //         removeEventListener: jest.fn(),
   //         dispatchEvent: jest.fn(),
