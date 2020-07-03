77e99267c05b38192c392f54dadb16584408b3cf
joris v.d. sluis <jjori8@hotmail.com>
2020-07-03 17:25:22 +0200

    prettify

--- frontend/src/components/implementation/ImplementationCard/index.tsx
+++ frontend/src/components/implementation/ImplementationCard/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import {ImplementationDTO} from "cloudsourced-api";
-import {Card, Button} from "antd";
-import {formatUser} from "../../../formatters/user";
-import {RouteComponentProps, withRouter, Link} from "react-router-dom";
+import { ImplementationDTO } from "cloudsourced-api";
+import { Card, Button } from "antd";
+import { formatUser } from "../../../formatters/user";
+import { RouteComponentProps, withRouter, Link } from "react-router-dom";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import {dark} from "react-syntax-highlighter/dist/esm/styles/prism";
-import {docco} from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { docco } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
 interface IProps extends RouteComponentProps {
   codeLanguage: string;
@@ -16,30 +16,27 @@ const ImplementationCard: React.FC<IProps> = (props) => {
   return (
     <Card>
       <div>
-        <SyntaxHighlighter language={props.codeLanguage}
-                           style={docco}
-
-        >
-
+        <SyntaxHighlighter language={props.codeLanguage} style={docco}>
           {props.impl.code!}
         </SyntaxHighlighter>
-
       </div>
-      <div style={{marginTop: 20, display: 'flex', alignItems: 'end'}}>
+      <div style={{ marginTop: 20, display: "flex", alignItems: "end" }}>
         <div>
-        <span>
-          by&nbsp;
-        </span>
+          <span>by&nbsp;</span>
           <Link to={`/user/${props.impl.user!.id}`}>
             {formatUser(props.impl.user!)}
           </Link>
         </div>
-        <div style={{flex: 1}}/>
+        <div style={{ flex: 1 }} />
 
         <div>
-          <Button onClick={() => {
-            props.history.push(`/projects/1/features/1/implementations/${props.impl.id}`);
-          }}>
+          <Button
+            onClick={() => {
+              props.history.push(
+                `/projects/1/features/1/implementations/${props.impl.id}`
+              );
+            }}
+          >
             To detail view
           </Button>
         </div>
@@ -49,5 +46,3 @@ const ImplementationCard: React.FC<IProps> = (props) => {
 };
 
 export default withRouter(ImplementationCard);
-
-
