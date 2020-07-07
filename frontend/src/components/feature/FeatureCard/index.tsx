b3e9c26675ae08c66a26956adf62192844eb1179
jjori <jjori8@hotmail.com>
2020-07-08 00:46:56 +0200

    added features finished icon to detail

--- frontend/src/components/feature/FeatureCard/index.tsx
+++ frontend/src/components/feature/FeatureCard/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
-import { Card, Button, Statistic } from "antd";
+import {Card, Button, Statistic, Tooltip} from "antd";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 import { RouteComponentProps, withRouter } from "react-router-dom";
 import { FeatureDTO } from "cloudsourced-api";
 import { DollarOutlined } from "@ant-design/icons";
 import {languages} from "../../../core/languages";
+import feature from "../../../core/DataContext/feature";
+import {CheckOutlined} from "@ant-design/icons/lib";
 
 interface IProps extends RouteComponentProps {
   data: FeatureDTO;
@@ -21,6 +23,7 @@ const FeatureCard: React.FC<IProps> = (props) => {
     codeLanguage,
     codePreview,
     points,
+    finishedAt
   } = props.data;
   const projectId = props.projectId;
 
@@ -33,7 +36,11 @@ const FeatureCard: React.FC<IProps> = (props) => {
       title={ <div >{name} <i className={'devicon-' +Object.keys(languages).find(
           key => Object.keys(languages).indexOf(key) === Object.values(languages).indexOf( codeLanguage as languages ))
        + '-plain colored'}/></div>}
-      extra={
+      extra={ finishedAt ?
+          <Tooltip key="finish" title="This feature is finished!">
+            <CheckOutlined  style={{fontSize: "2em", color: "green"}}/>
+          </Tooltip>
+          :
         <Statistic
           value={points}
           precision={0}
