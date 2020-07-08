aae95d90d561d3bb6b4946daeb2c0f05c05cc5b5
Gavin den hollander <gavindhollander@gmail.com>
2020-07-09 01:22:25 +0200

    projectService test

--- backend/src/test/java/io/cloudsourced/api/cloudsourcedapi/service/ProjectServiceTests.java
+++ backend/src/test/java/io/cloudsourced/api/cloudsourcedapi/service/ProjectServiceTests.java
@@ -1,8 +1,13 @@
 package io.cloudsourced.api.cloudsourcedapi.service;
 
+import io.cloudsourced.api.cloudsourcedapi.Default.Authentication.AuthenticatedUserBean;
+import io.cloudsourced.api.cloudsourcedapi.Default.Exception.NotFoundException;
 import io.cloudsourced.api.cloudsourcedapi.Entity.Project;
+import io.cloudsourced.api.cloudsourcedapi.Entity.User;
 import io.cloudsourced.api.cloudsourcedapi.Persistence.ProjectRepository;
 import io.cloudsourced.api.cloudsourcedapi.Service.ProjectService;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
@@ -12,11 +17,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class ProjectServiceTests {
@@ -26,6 +34,9 @@ public class ProjectServiceTests {
 
     @Mock
     private ProjectRepository projectRepository;
+
+    @Mock
+    AuthenticatedUserBean authenticatedUserProvider;
 
     @Test
     public void SearchProjectTestShouldFail(){
@@ -70,6 +81,94 @@ public class ProjectServiceTests {
 
     }
 
+    @Test
+    void givenUserAuthenticated_saveWithUser(){
+        final Project project = new Project();
 
+        when(authenticatedUserProvider.getUser()).thenReturn(new User());
+        when(projectRepository.save(project)).thenReturn(project);
+
+        assertEquals(project, projectService.saveWithUser(project));
+    }
+
+    @Test
+    void givenNonExistingId_getProjectDetailById(){
+
+        when(projectRepository.findById(1L)).thenReturn(Optional.empty());
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            projectService.getProjectDetailById(1L);
+        });
+    }
+
+    @Test
+    void givenNUllgetArchivedAt_getProjectDetailById(){
+        User AuthUserMock = mock(User.class);
+        User OtherUserMock = mock(User.class);
+        Project project = mock(Project.class);
+        when(projectRepository.findById(1L)).thenReturn(Optional.of(project));
+        when(authenticatedUserProvider.getUser()).thenReturn(AuthUserMock);
+
+        when(project.getArchivedAt()).thenReturn(Instant.now());
+        when(AuthUserMock.getId()).thenReturn(1L);
+        when(OtherUserMock.getId()).thenReturn(2L);
+        when(project.getUser()).thenReturn(OtherUserMock);
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            projectService.getProjectDetailById(1L);
+        });
+    }
+
+    @Test
+    void givenValidId_getProjectDetailById(){
+        User AuthUserMock = mock(User.class);
+        Project project = mock(Project.class);
+        when(projectRepository.findById(1L)).thenReturn(Optional.of(project));
+        when(authenticatedUserProvider.getUser()).thenReturn(AuthUserMock);
+
+        when(project.getArchivedAt()).thenReturn(Instant.now());
+        when(AuthUserMock.getId()).thenReturn(1L);
+        when(project.getUser()).thenReturn(AuthUserMock);
+
+        assertEquals(project, projectService.getProjectDetailById(1L));
+
+    }
+
+    @Test
+    void getAll_test(){
+        List<Project> lp = new ArrayList<Project>();
+        when(projectRepository.getAllUnfinished()).thenReturn(lp);
+        assertEquals(lp, projectService.getAll());
+
+    }
+
+    @Test
+    void getAllByAuthenticatedUser_test(){
+        List<Project> lp = new ArrayList<Project>();
+        User user = new User();
+        when(authenticatedUserProvider.getUser()).thenReturn(user);
+        when(projectRepository.findByUser(user)).thenReturn(lp);
+        assertEquals(lp, projectService.getProjectsByUser());
+    }
+
+    @Test
+    void getAllByUserId_test(){
+        List<Project> lp = new ArrayList<Project>();
+       when(projectRepository.byUserId(1L)).thenReturn(lp);
+       assertEquals(lp, projectService.getProjectsByUserId(1L));
+    }
+
+    @Test
+    void finishProject_test(){
+        Project p = mock(Project.class);
+        when(projectRepository.save(p)).thenReturn(p);
+        assertEquals(p, projectService.finishProject(p));
+    }
+
+    @Test
+    void archiveProject_test(){
+        Project p = mock(Project.class);
+        when(projectRepository.save(p)).thenReturn(p);
+        assertEquals(p, projectService.archiveProject(p));
+    }
 
 }
